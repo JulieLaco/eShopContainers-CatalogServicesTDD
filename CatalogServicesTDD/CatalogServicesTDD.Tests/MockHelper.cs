0a701ddae084d53fbf7557de728b91fd7e1db6ff
Julie LACOGNATA <julie.lacognata@infeeny.com>
2020-04-06 15:01:25 +0200

    Remove useless packages nuGet

---
+++ CatalogServicesTDD/CatalogServicesTDD.Tests/MockHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CatalogServicesTDD.Tests
+{
+    public class MockHelper
+    {
+        //mock la base 
+        
+    }
+}
