3df8cb76e9c8ad30c7c1372f34f4675376c711aa
Julie LACOGNATA <julie.lacognata@infeeny.com>
2020-04-06 15:02:11 +0200

    Remove useless using

--- CatalogServicesTDD/CatalogServicesTDD.Tests/JlaCatalogTests.cs
+++ CatalogServicesTDD/CatalogServicesTDD.Tests/JlaCatalogTests.cs
@@ -1,5 +1,4 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Rhino.Mocks;
 
 namespace CatalogServicesTDD.Tests
 {
--- CatalogServicesTDD/CatalogServicesTDD.Tests/KanCatalogTests.cs
+++ CatalogServicesTDD/CatalogServicesTDD.Tests/KanCatalogTests.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-using Rhino.Mocks;
 
 namespace CatalogServicesTDD.Tests
 {
