d4d80e84179bc3f1dd40b3274fc15d50f246ade0
Julie LACOGNATA <julie.lacognata@infeeny.com>
2020-04-06 14:46:18 +0200

    Initialize project CatalogTDD

---
+++ CatalogServicesTDD/CatalogServicesTDD.Tests/JlaCatalogTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rhino.Mocks;
+
+namespace CatalogServicesTDD.Tests
+{
+    [TestClass]
+    public class JlaCatalogTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ CatalogServicesTDD/CatalogServicesTDD.Tests/KanCatalogTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Rhino.Mocks;
+
+namespace CatalogServicesTDD.Tests
+{
+    [TestClass]
+    class KanCatalogTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
